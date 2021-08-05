3298e4c1e628cac8fc07b1b8f09c6ece166a796e
Jun Ni <tweeg.ni@gmail.com>
2021-08-05 15:57:54 +0800

    v-bind

--- v2/js/002-main.js
+++ v2/js/002-main.js
@@ -2,7 +2,7 @@
 var vue = new Vue({
   el: "#vue",                 // DOM 选择器，选择 HTML 元素
   data: {
-    greeting: "<font color='gray'>Hello</font>, <span style='color: red;'>vue!</span>",
+    greeting: "<font color='gray'>Hello</font>, <span style='color: red;'> HTML code</span> with vue!",
     price: 100,
   },
   filters: {
