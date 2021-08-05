b07b33ed0ed304558a8212bcf049d4b9a8e8d459
Jun Ni <tweeg.ni@gmail.com>
2021-08-05 18:07:48 +0800

    expression

---
+++ v2/js/000-main.js
@@ -0,0 +1,13 @@
+/* Vue 实例 */
+var vue = new Vue({
+  el: "#vue",                 // DOM 选择器，选择 HTML 元素
+  
+  data: {
+    
+
+  },
+
+  filters: {
+    
+  }
+});
--- v2/js/002-main.js
+++ v2/js/002-main.js
@@ -1,12 +1,16 @@
 /* Vue 实例 */
 var vue = new Vue({
-  el: "#vue",                 // DOM 选择器，选择 HTML 元素
+  el: "#vue",                 // DOM 选择器，选择 HTML 元素绑定 vue 实例
+
   data: {
-    greeting: "<font color='gray'>Hello</font>, <span style='color: red;'> HTML code</span> with vue!",
+    // 002-hello-world-v2-seperated.html
+    greetingHTML: "<font color='gray'>Hello</font>, <span style='color: red;'> HTML code</span> with vue!",
     price: 100,
+
   },
+
   filters: {
-    currencyChineseYuan(value) {
+    currencyChineseYuan: function (value) {
       return "¥" + value;
     }
   }
